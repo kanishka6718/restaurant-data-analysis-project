182b8c32df8be1228271353cebcf09f2174960f0
Kanishka <64245469+kanishka6718@users.noreply.github.com>
2025-05-31 14:30:52 +0530

    Update Exploring order table.sql

--- Exploring order table.sql
+++ Exploring order table.sql
@@ -1,13 +1,20 @@
 
+# how many items (not unique orders) were ordered in total within this date range? 
 
+SELECT COUNT(order_id) FROM  restaurant_db.order_details
+WHERE order_date BETWEEN '2023-01-01' AND '2023-01-08';
+
+
+# which orders had the most items during that week.
+  
 SELECT COUNT(order_details_id) AS item_ordered, order_id FROM restaurant_db.order_details
 WHERE order_date BETWEEN '2023-01-01' AND '2023-01-08'
 GROUP BY order_id
 ORDER BY item_ordered DESC ;
 
-SELECT COUNT(order_id) FROM  restaurant_db.order_details
-WHERE order_date BETWEEN '2023-01-01' AND '2023-01-08';
 
+# How many orders had more than 12 items?
+  
 SELECT Order_id FROM (SELECT COUNT(order_details_id) AS item_ordered, order_id FROM restaurant_db.order_details
 WHERE order_date BETWEEN '2023-01-01' AND '2023-01-08'
 GROUP BY order_id) AS S
